72732157ad5842a24232f52647342ed9920b79e3
czs007 <czs@alumni.hust.edu.cn>
2020-08-19 10:38:22 +0800

    add new interface

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -1,8 +1,6 @@
 package types
 
-import (
-	"context"
-)
+import "context"
 
 type Key = []byte
 type Value = []byte
@@ -14,6 +12,7 @@ const (
 	TIKVDriver  DriverType = "TIKV"
 )
 
+/*
 type Store interface {
 	Get(ctx context.Context, key Key, timestamp Timestamp) (Value, error)
 	BatchGet(ctx context.Context, keys [] Key, timestamp Timestamp) ([]Value, error)
@@ -22,4 +21,45 @@ type Store interface {
 	Delete(ctx context.Context, key Key, timestamp Timestamp) error
 	BatchDelete(ctx context.Context, keys []Key, timestamp Timestamp) error
 	Close() error
+}
+*/
+
+type StoreEngine interface {
+	PUT(ctx context.Context, key Key, value Value)
+	GET(ctx context.Context, key Key) Value
+
+	GetLatest(ctx context.Context, key Key, withValue bool) (Key, Value)
+	GetAll(ctx context.Context, key Key, withValue bool) ([]Key, []Value)
+	Scan(ctx context.Context, keyStart Key, keyEnd Key, withValue bool) ([]Key, []Value)
+
+	Delete(ctx context.Context, key Key)
+	DeleteAll(ctx context.Context, key Key)
+	RangeDelete(ctx context.Context, keyStart Key, keyEnd Key)
+}
+
+type Store interface {
+	RawBatchGet(ctx context.Context, key Key) []Value
+	RawGet(ctx context.Context, key Key) Value
+	RawPut(ctx context.Context, key Key, value Value)
+
+	RawDeleteAll(ctx context.Context, key Key)
+	RawDelete(ctx context.Context, key Key)
+
+	Get(ctx context.Context, key Key, timestamp Timestamp) Value
+	BatchGet(ctx context.Context, keys []Key, timestamp Timestamp) []Value
+
+	GetAll(ctx context.Context, key Key, withValue bool) ([]Timestamp, []Key, []Value)
+
+	ScanLE(ctx context.Context, key Key, timestamp Timestamp, withValue bool) ([]Timestamp, []Key, []Value)
+	ScanGE(ctx context.Context, key Key, timestamp Timestamp, withValue bool) ([]Timestamp, []Key, []Value)
+	ScanRange(ctx context.Context, key Key, start Timestamp, end Timestamp, withValue bool) ([]Timestamp, []Key, []Value)
+
+	PUT(ctx context.Context, key Key, value Value, timestamp Timestamp, suffix string)
+
+	DeleteLE(ctx context.Context, key Key, timestamp Timestamp)
+	DeleteGE(ctx context.Context, key Key, timestamp Timestamp)
+	RangeDelete(ctx context.Context, key Key, start Timestamp, end Timestamp)
+
+	LogPut(ctx context.Context, key Key, value Value, timestamp Timestamp, suffix string)
+	LogFetch(ctx context.Context, start Timestamp, end Timestamp, channels []int)
 }
